7c2dd39891fb7793930169d4ac164df6258c9451
Nate Koike <nkoike1998@gmail.com>
2021-07-29 22:26:59 -1000

    rewrote lab3

---
+++ Lab3/lab3.js
@@ -0,0 +1,33 @@
+/*
+long comment
+*/
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+function reverse(string) {
+  let reversed = ''
+
+  for(let i = string.length - 1; i >= 0; i--) {
+    reversed += string[i]
+  }
+
+  return reversed
+}
+
+function main() {
+  readline.question(
+    "Enter a number to serve as an upper bound for the summation of perfect squares: ",
+    upper_int => {
+      let sum = 0
+      for (let val = 1; val <= upper_int; val++)
+        sum += Math.pow(val, 2)
+
+      console.log(sum)
+      console.log(reverse(sum.toString()))
+  })
+}
+
+main()
