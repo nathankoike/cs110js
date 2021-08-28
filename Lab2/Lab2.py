887e469bfb22faa13d280a9dfe67099e2a86424f
Nate Koike <nkoike1998@gmail.com>
2021-08-28 03:23:13 -1000

    removed python files

--- Lab2/Lab2.py
+++
@@ -1,9 +0,0 @@
-
-
-def main():
-    sentence = str(input("Write a sentence: "))
-    print(sentence[0:sentence.index(" ")])
-    print(sentence[(sentence.index(" ")+1):])
-
-if __name__ == "__main__":
-    main()
